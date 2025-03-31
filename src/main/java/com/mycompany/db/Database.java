b4da2e0117ac3096f3453813111554e02c9900f3
Luis Cubillos <luiscubillosq@outlook.com>
2025-03-31 16:12:01 -0500

    Created 'Database.java' file with some information inside and the java package 'com.mycompany.db' which contains the 'Database.java' file

---
+++ src/main/java/com/mycompany/db/Database.java
@@ -0,0 +1,12 @@
+package com.mycompany.db;
+
+import java.sql.Connection;
+
+
+public class Database {
+    
+    protected Connection conexion;
+    private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private final String DB_URL = "jdbc:mysql://localhost/"
+    
+}
